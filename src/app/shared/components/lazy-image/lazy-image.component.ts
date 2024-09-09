10fe92858beec856e510b844c8b46e474ace98a0
SergioVD <sergiveldi@gmail.com>
2024-09-09 09:35:53 +0200

    LazyImage

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styleUrls: ['./lazy-image.component.css'],
+})
+export class LazyImageComponent {
+  @Input() public urlImage!: string;
+  @Input() public alt!: string;
+  public hasLoaded: boolean = false;
+  ngOnInit() {
+    if (!this.urlImage) throw new Error('Url property is required');
+  }
+  onLoad() {
+    this.hasLoaded = true;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,12 @@
-
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 @NgModule({
-  declarations: [
-    
-    SearchBoxComponent,
-    SidebarComponent
-  ],
+  declarations: [SearchBoxComponent, SidebarComponent, LazyImageComponent],
   imports: [CommonModule],
-  exports:[SearchBoxComponent,SidebarComponent]
+  exports: [SearchBoxComponent, SidebarComponent, LazyImageComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
